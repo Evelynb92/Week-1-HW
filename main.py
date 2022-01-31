e002b2ca7708e13c4391761003a5c8de7a4cede4
EvelynBrooks1 <98491379+Evelynb92@users.noreply.github.com>
2022-01-31 16:16:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+name = "Evelyn"
+print('Hello my Name is...',name)
+x, y = 29 , 'August 9th, 1992'
+print('I was born on', y, 'and I am',x, 'years old.')
+z =135.6
+print('I currently weigh',z)
+w =5
+q =3
+print('I stand at',w, 'feet and',q, 'inches tall.')
+message = 'My Zodiac sign is a Leo.'
+print(message)
+print('My favorite number is', 10-3)
+print('My favorite hobby is')
+u = 5
+v = 10
+if u < v:
+ print('traveling.')
+ s ='extrovert'
+t ='introvert'
+print('I would say I am a mix of an', s, 'and', t, 'depending on the day.')
+myset = ['impatient', 'adaptable', 'honest', 'chatty', 'easy-going', 'loyal', 'friendly']
+print('Some would best describe me as...')
+for description in myset:
+    print(description, end= ", ")
